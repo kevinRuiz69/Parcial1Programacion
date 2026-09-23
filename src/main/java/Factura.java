a59a018b16c5637fa41e81e18e1bed215a8cb0c9
juanmburbanol-collab <juanm.burbanol@uqvirtual.edu.cogit config --global user.name juanm.burbanol@uqvirtual.edu.co2>
2026-09-23 02:17:41 -0500

    .

--- src/main/java/Factura.java
+++ src/main/java/Factura.java
@@ -1,5 +1,80 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factura {
 
+    //atributos
+    private String codigoProducto;
+    private LocalDate fechaRealizacion;
+    private double valorTotal;
+
+    //RELACION DE FACTURA CON METODOPAGO
+    private MetodoPago metodoPago;
+
+    //RELACION DE FACTURA CON DETALLEFACTURA
+    private List<DetalleFactura> listaDetalleFactura;
+
+    //constructor
+    public Factura(String codigoProducto, LocalDate fechaRealizacion, double valorTotal, MetodoPago metodoPago) {
+        this.codigoProducto = codigoProducto;
+        this.fechaRealizacion = fechaRealizacion;
+        this.valorTotal = valorTotal;
+        this.metodoPago = metodoPago;
+        this.listaDetalleFactura = new ArrayList<>();
+    }
+
+    //getters y setters
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getFechaRealizacion() {
+        return fechaRealizacion;
+    }
+
+    public void setFechaRealizacion(LocalDate fechaRealizacion) {
+        this.fechaRealizacion = fechaRealizacion;
+    }
+
+    public String getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(String codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public List<DetalleFactura> getListaDetalleFactura() {
+        return listaDetalleFactura;
+    }
+
+    public void setListaDetalleFactura(List<DetalleFactura> listaDetalleFactura) {
+        this.listaDetalleFactura = listaDetalleFactura;
+    }
 
 
+    //override
+
+
+    @Override
+    public String toString() {
+        return "codigoProducto: " + codigoProducto +
+                ", fechaRealizacion: " + fechaRealizacion +
+                ", valorTotal: " + valorTotal +
+                ", metodoPago: " + metodoPago +
+                ", listaDetalleFactura: " + listaDetalleFactura;
+    }
 }
--- src/main/java/MetodoPago.java
+++ src/main/java/MetodoPago.java
@@ -1,2 +1,7 @@
 public enum MetodoPago {
+
+    TARJETA,
+    TRANSFERENCIA,
+    EFECTIVO
+
 }
