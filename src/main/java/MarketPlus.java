5b2265a94fa9d72c21658549bd0c9d9674b452a9
kevinRuiz69 <kevina.ruizc@uqvirtual.edu.co>
2026-09-23 13:32:58 -0500

    MarketPlus base

--- src/main/java/MarketPlus.java
+++ src/main/java/MarketPlus.java
@@ -1,2 +1,70 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarketPlus {
+    private String nombreComercial;
+    private String direccion;
+    private String telefono;
+    //Relaciones
+    private List<Producto> listaProductos;
+    private List<Cliente> listaClientes;
+    private List<Factura> listaFacturas;
+
+    public MarketPlus(String nombreComercial, String direccion, String telefono) {
+        this.nombreComercial = nombreComercial;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.listaProductos = new ArrayList<>();
+        this.listaClientes = new ArrayList<>();
+        this.listaFacturas = new ArrayList<>();
+    }
+
+    public String getNombreComercial() {
+        return nombreComercial;
+    }
+
+    public void setNombreComercial(String nombreComercial) {
+        this.nombreComercial = nombreComercial;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public List<Factura> getListaFacturas() {
+        return listaFacturas;
+    }
+
+    public void setListaFacturas(List<Factura> listaFacturas) {
+        this.listaFacturas = listaFacturas;
+    }
 }
